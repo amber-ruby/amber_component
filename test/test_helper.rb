a78901956b2db0505707ddc866b33a799b2ec3c5
GarbusUchiha <p.garbicz@espago.com>
2022-07-25 00:07:29 +0200

    simplecov config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_group 'Amber Components', 'lib/'
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require 'amber_components'
 
 require 'minitest/autorun'
 require 'shoulda-context'
 require 'byebug'
-require 'simplecov'
+
 
 require 'haml'
 require 'nokogiri'
 
-SimpleCov.start do
-  add_filter '/test/'
-  add_group 'Amber Components', '/lib/'
-end
+
 
 # Load all files in the test/fixtures directory
 Dir['./test/fixtures/*_component.rb'].each do |file|
