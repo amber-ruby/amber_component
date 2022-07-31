98684d83619bb868351b82edc3a46378202cb45b
Mateusz Drewniak <matmg24@gmail.com>
2022-08-01 00:51:43 +0200

    Add a code coverage badge

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+[![license](https://img.shields.io/badge/License-MIT-purple.svg)](LICENSE)
 ![CI badge](https://github.com/amber-ruby/amber_component/actions/workflows/ci_ruby.yml/badge.svg)
+![Coverage Badge](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/Verseth/6a095c79278b074d79feaa4f8ceeb2a8/raw/amber_component__heads_main.json)
 
 # AmberComponent
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-cobertura'
 
 ::SimpleCov.start do
   add_filter '/test/'
   add_group 'Amber Component', 'lib/'
 end
+
+::SimpleCov.formatter = ::SimpleCov::Formatter::MultiFormatter.new([
+  ::SimpleCov::Formatter::HTMLFormatter,
+  ::SimpleCov::Formatter::CoberturaFormatter
+])
 
 $LOAD_PATH.unshift ::File.expand_path("../lib", __dir__)
 require 'amber_component'
