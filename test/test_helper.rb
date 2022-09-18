be6e1c63512dbda44ae8f6685a8062d893b3e385
Mateusz Drewniak <matmg24@gmail.com>
2022-09-18 16:29:53 +0200

    Calculate code coverage only once

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-require 'simplecov-cobertura'
+if !::ENV['COVERAGE'].nil? && ::ENV['COVERAGE'].length.positive?
+  require 'simplecov'
+  require 'simplecov-cobertura'
 
-::SimpleCov.start do
-  add_filter '/test/'
-  add_group 'Amber Component', 'lib/'
+  ::SimpleCov.start do
+    add_filter '/test/'
+    add_group 'Amber Component', 'lib/'
+  end
+
+  ::SimpleCov.formatter = ::SimpleCov::Formatter::MultiFormatter.new([
+    ::SimpleCov::Formatter::HTMLFormatter,
+    ::SimpleCov::Formatter::CoberturaFormatter
+  ])
 end
-
-::SimpleCov.formatter = ::SimpleCov::Formatter::MultiFormatter.new([
-  ::SimpleCov::Formatter::HTMLFormatter,
-  ::SimpleCov::Formatter::CoberturaFormatter
-])
 
 $LOAD_PATH.unshift ::File.expand_path("../lib", __dir__)
 require 'amber_component'
