2f450051a31e4145e557336efe1a68897fc7f896
Mateusz Drewniak <matmg24@gmail.com>
2022-09-25 13:59:53 +0200

    Improve prop typechecking

--- lib/amber_component/prop_definition.rb
+++ lib/amber_component/prop_definition.rb
@@ -12,19 +12,24 @@ module ::AmberComponent
     attr_reader :required
     # @return [Object, Proc, nil]
     attr_reader :default
+    # @return [Boolean]
+    attr_reader :allow_nil
 
     # @param name [Symbol]
     # @param type [Class, nil]
     # @param required [Boolean]
     # @param default [Object, Proc, nil]
-    def initialize(name:, type: nil, required: false, default: nil)
+    # @param allow_nil [Boolean]
+    def initialize(name:, type: nil, required: false, default: nil, allow_nil: false)
       @name = name
       @type = type
       @required = required
       @default = default
+      @allow_nil = allow_nil
     end
 
     alias required? required
+    alias allow_nil? allow_nil
 
     # @return [Boolean]
     def type?
--- lib/amber_component/props.rb
+++ lib/amber_component/props.rb
@@ -12,21 +12,47 @@ module ::AmberComponent
       attr_reader :prop_definitions
 
       # @param names [Array<Symbol>]
-      # @param type [Class]
+      # @param type [Class, nil]
       # @param required [Boolean]
       # @param default [Object, Proc, nil]
-      def prop(*names, type: nil, required: false, default: nil)
+      # @param allow_nil [Boolean]
+      def prop(*names, type: nil, required: false, default: nil, allow_nil: false)
         @prop_definitions ||= {}
+        include(@prop_methods_module = ::Module.new) if @prop_methods_module.nil?
 
         names.each do |name|
-          attr_accessor name
-
-          @prop_definitions[name] = PropDefinition.new(
+          @prop_definitions[name] = prop_def = PropDefinition.new(
             name: name,
             type: type,
             required: required,
-            default: default
+            default: default,
+            allow_nil: allow_nil
           )
+          raise IncorrectPropTypeError, <<~MSG unless type.nil? || type.is_a?(::Class)
+            `type` should be a class but received `#{type.inspect}` (`#{type.class}`)
+          MSG
+
+          @prop_methods_module.attr_reader name
+          next @prop_methods_module.attr_writer(name) unless prop_def.type?
+
+          @prop_methods_module.class_eval( # rubocop:disable Style/DocumentDynamicEvalDefinition
+            # def phone=(val)
+            #   raise IncorrectPropTypeError, <<~MSG unless val.nil? || val.is_a?(String)
+            #     #{self.class} received `#{val.class}` instead of `String` for `phone` prop
+            #   MSG
+            #
+            #   @phone = val
+            # end
+            <<~RUB, __FILE__, __LINE__ + 1
+              def #{name}=(val)
+                raise IncorrectPropTypeError, <<~MSG unless #{allow_nil ? 'val.nil? ||' : nil} val.is_a?(#{prop_def.type})
+                  \#{self.class} received `\#{val.class}` instead of `#{prop_def.type}` for `#{name}` prop
+                MSG
+
+                @#{name} = val
+              end
+            RUB
+          ) # rubocop:disable Layout/HeredocArgumentClosingParenthesis
         end
       end
 
@@ -75,10 +101,6 @@ module ::AmberComponent
           next unless prop_present
 
           value = props[name]
-          raise IncorrectPropTypeError, <<~MSG if prop_def.type? && !value.is_a?(prop_def.type)
-            #{self.class} received `#{value.class}` instead of `#{prop_def.type}` for `#{name}` prop
-          MSG
-
           public_send(setter_name, value)
         end
 
