0d22143335d3828a2d64187062a0ca266c24e16a
Mateusz Drewniak <matmg24@gmail.com>
2022-11-06 18:33:54 +0100

    Add the `rails g component` command

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -4,4 +4,6 @@ require 'fileutils'
 require_relative 'amber_component_generator'
 
 # A Rails generator which creates a new Amber component.
-class ComponentGenerator < AmberComponentGenerator; end
+class ComponentGenerator < AmberComponentGenerator
+  source_root ::File.expand_path('templates', __dir__)
+end
