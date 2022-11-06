304c1b89464585c41d21e900af29a42d328d316d
Mateusz Drewniak <matmg24@gmail.com>
2022-11-06 19:41:51 +0100

    Add slim and haml templates

--- lib/generators/amber_component_generator.rb
+++ lib/generators/amber_component_generator.rb
@@ -7,16 +7,24 @@ class AmberComponentGenerator < ::Rails::Generators::NamedBase
   desc 'Generate a new component'
   source_root ::File.expand_path('templates', __dir__)
 
+  # @return [Array<Symbol>]
+  VIEW_FORMATS = %i[html erb haml slim]
+
+  class_option view: :string
   # copy rake tasks
   def copy_tasks
+    @view_format = (options[:view] || :html).to_sym
+    @view_format = :html if @view_format == :erb
+
+    unless VIEW_FORMATS.include? @view_format
+      puts "No such view format as `#{@view_format}`"
+      error!
+    end
+
     template 'component.rb.erb', "app/components/#{file_path}.rb"
     template 'component_test.rb.erb', "test/components/#{file_path}_test.rb"
-    template 'view.html.erb.erb', "app/components/#{file_path}/view.html.erb"
-    if defined?(::SassC)
-      template 'style.scss.erb', "app/components/#{file_path}/style.scss"
-    else
-      template 'style.css.erb', "app/components/#{file_path}/style.css"
-    end
+    create_stylesheet
+    create_view
   end
 
   def file_name
@@ -24,5 +32,28 @@ class AmberComponentGenerator < ::Rails::Generators::NamedBase
     return name if name.end_with? '_component'
 
     "#{name}_component"
+  end
+
+  private
+
+  # @return [void]
+  def create_view
+    case @view_format
+    when :slim
+      template 'view.slim.erb', "app/components/#{file_path}/view.slim"
+    when :haml
+      template 'view.haml.erb', "app/components/#{file_path}/view.haml"
+    else
+      template 'view.html.erb.erb', "app/components/#{file_path}/view.html.erb"
+    end
+  end
+
+  # @return [void]
+  def create_stylesheet
+    if defined?(::SassC)
+      template 'style.scss.erb', "app/components/#{file_path}/style.scss"
+    else
+      template 'style.css.erb', "app/components/#{file_path}/style.css"
+    end
   end
 end
